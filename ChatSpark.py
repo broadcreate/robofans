fc3109d5d4109c6e4a0d6b35ce084c44fec4ea4c
shengjun zhang <34327137+broadcreate@users.noreply.github.com>
2024-08-14 18:04:45 +0800

    Update ChatSpark.py

--- ChatSpark.py
+++ ChatSpark.py
@@ -5,9 +5,9 @@ import os
 SPARKAI_URL = 'wss://spark-api.xf-yun.com/v3.5/chat'
 #星火认知大模型调用秘钥信息，请前往讯飞开放平台控制台（https://console.xfyun.cn/services/bm35）查看
 print(os.environ)
-SPARKAI_APP_ID = os.environ['SPARKAI_APP_ID']  
-SPARKAI_API_SECRET = os.environ['SPARKAI_API_SECRET']  
-SPARKAI_API_KEY = os.environ['SPARKAI_API_KEY']  
+SPARKAI_APP_ID = os.getenv('SPARKAI_APP_ID') 
+SPARKAI_API_SECRET =os.getenv('SPARKAI_API_SECRET') 
+SPARKAI_API_KEY = os.getenv('SPARKAI_API_KEY')
 #星火认知大模型Spark Max的domain值，其他版本大模型domain值请前往文档（https://www.xfyun.cn/doc/spark/Web.html）查看
 SPARKAI_DOMAIN = 'generalv3.5'
 
